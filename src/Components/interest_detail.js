47192d0de6eba7aad2757e9566089882007a3588
Emily Bolten <emilybolten@gmail.com>
2021-04-22 16:58:23 -0400

    get details for specific skills or interests

--- src/Components/interest_detail.js
+++ src/Components/interest_detail.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 function InterestDeatil(props) {
+    // detail item
+    let currentItem = getItem(props.history.location.pathname.split('/')[props.history.location.pathname.split('/').length-1],props.store.getState().interests);
+
     return(
-        <div>
-            
+        <div id="interest-details">
+            <h1 id="home-welcome">{currentItem.name}</h1>
         </div>
     )
+}
+
+// get interest
+function getItem(id,store) {
+    for (var i = 0; i < store.interests.length; i++) {
+        if (store.interests[i].id === parseInt(id)) {
+            return store.interests[i];
+        }
+    }
+    return id.toString();
 }
 export default InterestDeatil;
--- src/Components/skill_detail.js
+++ src/Components/skill_detail.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-function SkillDeatil() {
+function SkillDeatil(props) {
+    // detail item
+    let currentItem = getItem(props.history.location.pathname.split('/')[props.history.location.pathname.split('/').length-1],props.store.getState().skills);
     return(
-        <div>
+        <div id="skill-details">
+            <h1 id="home-welcome">{currentItem.name}</h1>
         </div>
     )
+}
+
+// get skill
+function getItem(id,store) {
+    for (var i = 0; i < store.skills.length; i++) {
+        if (store.skills[i].id === parseInt(id)) {
+            return store.skills[i];
+        }
+    }
+    return id.toString();
 }
 export default SkillDeatil;
